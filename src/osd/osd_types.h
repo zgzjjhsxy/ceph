zenglg/ceph-ci

osd: add a 'delete' flag to missing items and related functions
This will track deletes that were in the pg log and still need to be
performed during recovery. Note that with these deleted objects we may
not have an accurate 'have' version, since the object may have already
been deleted locally, so tolerate this when examining divergent entries.

Signed-off-by: Josh Durgin <jdurgin@redhat.com>

src/osd/osd_types.h
@@ -3584,9 +3584,15 @@ inline ostream& operator<<(ostream& out, const pg_log_t& log)
  */
 struct pg_missing_item {
   eversion_t need, have;
-  pg_missing_item() {}
-  explicit pg_missing_item(eversion_t n) : need(n) {}  // have no old version
-  pg_missing_item(eversion_t n, eversion_t h) : need(n), have(h) {}
+  enum missing_flags_t {
+    FLAG_NONE = 0,
+    FLAG_DELETE = 1,
+  } flags;
+  pg_missing_item() : flags(FLAG_NONE) {}
+  explicit pg_missing_item(eversion_t n) : need(n), flags(FLAG_NONE) {}  // have no old version
+  pg_missing_item(eversion_t n, eversion_t h, bool is_delete=false) : need(n), have(h) {
+    set_delete(is_delete);
+  }
 
   void encode(bufferlist& bl) const {
     ::encode(need, bl);
@@ -3596,18 +3602,48 @@ struct pg_missing_item {
     ::decode(need, bl);
     ::decode(have, bl);
   }
+
+  void set_delete(bool is_delete) {
+    flags = is_delete ? FLAG_DELETE : FLAG_NONE;
+  }
+
+  bool is_delete() const {
+    return (flags & FLAG_DELETE) == FLAG_DELETE;
+  }
+
+  void encode_with_flags(bufferlist& bl) const {
+    encode(bl);
+    ::encode(static_cast<uint8_t>(flags), bl);
+  }
+
+  void decode_with_flags(bufferlist::iterator& bl) {
+    decode(bl);
+    // no versioning on this, but it's stored in a single omap value,
+    // so just check for the end of the bufferlist
+    if (!bl.end()) {
+      uint8_t f;
+      ::decode(f, bl);
+      flags = static_cast<missing_flags_t>(f);
+    }
+  }
+
   void dump(Formatter *f) const {
     f->dump_stream("need") << need;
     f->dump_stream("have") << have;
+    f->dump_stream("flags") << (flags == FLAG_NONE ? "none" : "delete");
   }
   static void generate_test_instances(list<pg_missing_item*>& o) {
     o.push_back(new pg_missing_item);
     o.push_back(new pg_missing_item);
     o.back()->need = eversion_t(1, 2);
     o.back()->have = eversion_t(1, 1);
+    o.push_back(new pg_missing_item);
+    o.back()->need = eversion_t(3, 5);
+    o.back()->have = eversion_t(3, 4);
+    o.back()->flags = FLAG_DELETE;
   }
   bool operator==(const pg_missing_item &rhs) const {
-    return need == rhs.need && have == rhs.have;
+    return need == rhs.need && have == rhs.have && flags == rhs.flags;
   }
   bool operator!=(const pg_missing_item &rhs) const {
     return !(*this == rhs);
@@ -3730,43 +3766,40 @@ public:
    * assumes missing is accurate up through the previous log entry.
    */
   void add_next_event(const pg_log_entry_t& e) {
-    if (e.is_update()) {
-      map<hobject_t, item>::iterator missing_it;
-      missing_it = missing.find(e.soid);
-      bool is_missing_divergent_item = missing_it != missing.end();
-      if (e.prior_version == eversion_t() || e.is_clone()) {
-	// new object.
-	if (is_missing_divergent_item) {  // use iterator
-	  rmissing.erase((missing_it->second).need.version);
-	  missing_it->second = item(e.version, eversion_t());  // .have = nil
-	} else  // create new element in missing map
-	  missing[e.soid] = item(e.version, eversion_t());     // .have = nil
-      } else if (is_missing_divergent_item) {
-	// already missing (prior).
+    map<hobject_t, item>::iterator missing_it;
+    missing_it = missing.find(e.soid);
+    bool is_missing_divergent_item = missing_it != missing.end();
+    if (e.prior_version == eversion_t() || e.is_clone()) {
+      // new object.
+      if (is_missing_divergent_item) {  // use iterator
 	rmissing.erase((missing_it->second).need.version);
-	(missing_it->second).need = e.version;  // leave .have unchanged.
-      } else if (e.is_backlog()) {
-	// May not have prior version
-	assert(0 == "these don't exist anymore");
-      } else {
-	// not missing, we must have prior_version (if any)
-	assert(!is_missing_divergent_item);
-	missing[e.soid] = item(e.version, e.prior_version);
-      }
-      rmissing[e.version.version] = e.soid;
-    } else if (e.is_delete()) {
-      rm(e.soid, e.version);
+	missing_it->second = item(e.version, eversion_t(), e.is_delete());  // .have = nil
+      } else  // create new element in missing map
+	missing[e.soid] = item(e.version, eversion_t(), e.is_delete());     // .have = nil
+    } else if (is_missing_divergent_item) {
+      // already missing (prior).
+      rmissing.erase((missing_it->second).need.version);
+      (missing_it->second).need = e.version;  // leave .have unchanged.
+      missing_it->second.set_delete(e.is_delete());
+    } else if (e.is_backlog()) {
+      // May not have prior version
+      assert(0 == "these don't exist anymore");
+    } else {
+      // not missing, we must have prior_version (if any)
+      assert(!is_missing_divergent_item);
+      missing[e.soid] = item(e.version, e.prior_version, e.is_delete());
     }
-
+    rmissing[e.version.version] = e.soid;
     tracker.changed(e.soid);
   }
 
-  void revise_need(hobject_t oid, eversion_t need) {
+  void revise_need(hobject_t oid, eversion_t need, bool is_delete) {
     if (missing.count(oid)) {
       rmissing.erase(missing[oid].need.version);
       missing[oid].need = need;            // no not adjust .have
+      missing[oid].set_delete(is_delete);
     } else {
-      missing[oid] = item(need, eversion_t());
+      missing[oid] = item(need, eversion_t(), is_delete);
     }
     rmissing[need.version] = oid;
 
@@ -3780,8 +3813,9 @@ public:
     }
   }
 
-  void add(const hobject_t& oid, eversion_t need, eversion_t have) {
-    missing[oid] = item(need, have);
+  void add(const hobject_t& oid, eversion_t need, eversion_t have,
+	   bool is_delete) {
+    missing[oid] = item(need, have, is_delete);
     rmissing[need.version] = oid;
     tracker.changed(oid);
   }
@@ -3801,7 +3835,7 @@ public:
   void got(const hobject_t& oid, eversion_t v) {
     std::map<hobject_t, item>::iterator p = missing.find(oid);
     assert(p != missing.end());
-    assert(p->second.need <= v);
+    assert(p->second.need <= v || p->second.is_delete());
     got(p);
   }
 
@@ -3820,7 +3854,8 @@ public:
 	 i != missing.end();
       ) {
       if ((i->first.get_hash() & mask) == child_pgid.m_seed) {
-	omissing->add(i->first, i->second.need, i->second.have);
+	omissing->add(i->first, i->second.need, i->second.have,
+		      i->second.is_delete());
 	rm(i++);
       } else {
 	++i;
@@ -3836,15 +3871,33 @@ public:
   }
 
   void encode(bufferlist &bl) const {
-    ENCODE_START(3, 2, bl);
+    ENCODE_START(4, 2, bl);
     ::encode(missing, bl);
+    // since pg_missing_item was not versioned, we encode the new flags
+    // field here explicitly
+    map<hobject_t, uint8_t> missing_flags;
+    for (const auto &p : missing) {
+      if (p.second.flags != pg_missing_item::FLAG_NONE) {
+	missing_flags.insert(make_pair(p.first,
+				       static_cast<uint8_t>(p.second.flags)));
+      }
+    }
+    ::encode(missing_flags, bl);
     ENCODE_FINISH(bl);
   }
   void decode(bufferlist::iterator &bl, int64_t pool = -1) {
     for (auto const &i: missing)
       tracker.changed(i.first);
-    DECODE_START_LEGACY_COMPAT_LEN(3, 2, 2, bl);
+    DECODE_START_LEGACY_COMPAT_LEN(4, 2, 2, bl);
     ::decode(missing, bl);
+    if (struct_v >= 4) {
+      map<hobject_t, uint8_t> missing_flags;
+      ::decode(missing_flags, bl);
+      for (const auto &p : missing_flags) {
+	assert(missing.find(p.first) != missing.end());
+	missing[p.first].flags = static_cast<pg_missing_item::missing_flags_t>(p.second);
+      }
+    }
     DECODE_FINISH(bl);
 
     if (struct_v < 3) {
@@ -3900,7 +3953,7 @@ public:
     o.push_back(new pg_missing_set);
     o.back()->add(
       hobject_t(object_t("foo"), "foo", 123, 456, 0, ""),
-      eversion_t(5, 6), eversion_t(5, 1));
+      eversion_t(5, 6), eversion_t(5, 1), true);
   }
   template <typename F>
   void get_changed(F &&f) const {

zgzjjhsxy/ceph

(cherry picked from commit fa037fb863532bca8de90a34042f3fe5162b46f2)
Conflicts: everything due to moves between .h and .cc and restructuring of missing set
Resolves: rhbz#1452780

src/osd/osd_types.h
@@ -2764,9 +2764,15 @@ inline ostream& operator<<(ostream& out, const pg_log_t& log)
 struct pg_missing_t {
   struct item {
     eversion_t need, have;
-    item() {}
-    explicit item(eversion_t n) : need(n) {}  // have no old version
-    item(eversion_t n, eversion_t h) : need(n), have(h) {}
+    enum missing_flags_t {
+      FLAG_NONE = 0,
+      FLAG_DELETE = 1,
+    } flags;
+    item() : flags(FLAG_NONE) {}
+    explicit item(eversion_t n) : need(n), flags(FLAG_NONE) {}  // have no old version
+    item(eversion_t n, eversion_t h, bool is_delete=false) : need(n), have(h) {
+      set_delete(is_delete);
+    }
 
     void encode(bufferlist& bl) const {
       ::encode(need, bl);
@@ -2776,15 +2782,45 @@ struct pg_missing_t {
       ::decode(need, bl);
       ::decode(have, bl);
     }
+
+    void set_delete(bool is_delete) {
+      flags = is_delete ? FLAG_DELETE : FLAG_NONE;
+    }
+
+    bool is_delete() const {
+      return (flags & FLAG_DELETE) == FLAG_DELETE;
+    }
+
+    void encode_with_flags(bufferlist& bl) const {
+      encode(bl);
+      ::encode(static_cast<uint8_t>(flags), bl);
+    }
+
+    void decode_with_flags(bufferlist::iterator& bl) {
+      decode(bl);
+      // no versioning on this, but it's stored in a single omap value,
+      // so just check for the end of the bufferlist
+      if (!bl.end()) {
+	uint8_t f;
+	::decode(f, bl);
+	flags = static_cast<missing_flags_t>(f);
+      }
+    }
+
     void dump(Formatter *f) const {
       f->dump_stream("need") << need;
       f->dump_stream("have") << have;
+      f->dump_stream("flags") << (flags == FLAG_NONE ? "none" : "delete");
     }
     static void generate_test_instances(list<item*>& o) {
       o.push_back(new item);
       o.push_back(new item);
       o.back()->need = eversion_t(1, 2);
       o.back()->have = eversion_t(1, 1);
+      o.push_back(new item);
+      o.back()->need = eversion_t(3, 5);
+      o.back()->have = eversion_t(3, 4);
+      o.back()->flags = FLAG_DELETE;
     }
   }; 
   WRITE_CLASS_ENCODER(item)
@@ -2799,9 +2835,9 @@ struct pg_missing_t {
   bool is_missing(const hobject_t& oid, eversion_t v) const;
   eversion_t have_old(const hobject_t& oid) const;
   void add_next_event(const pg_log_entry_t& e);
-  void revise_need(hobject_t oid, eversion_t need);
+  void revise_need(hobject_t oid, eversion_t need, bool is_delete);
   void revise_have(hobject_t oid, eversion_t have);
-  void add(const hobject_t& oid, eversion_t need, eversion_t have);
+  void add(const hobject_t& oid, eversion_t need, eversion_t have, bool is_delete);
   void rm(const hobject_t& oid, eversion_t v);
   void rm(const std::map<hobject_t, pg_missing_t::item, hobject_t::ComparatorWithDefault>::iterator &m);
   void got(const hobject_t& oid, eversion_t v);