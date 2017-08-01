3de9eef807b066f47e0ef55f08235ad4869921f0
Abhishek Varshney <abhishek.varshney@flipkart.com>
2017-08-01 14:46:52 +0000

    rgw : fix race in RGWCompleteMultipart
    Fixes : http://tracker.ceph.com/issues/20861
    
    Signed-off-by: Abhishek Varshney <abhishek.varshney@flipkart.com>
    (cherry picked from commit dd7dc62eaddb47447c01c03c87ad56cd7690693e)
    Signed-off-by: Matt Benjamin <mbenjamin@redhat.com>
    (cherry picked from commit 5a7ea2790c391544a3a8ea4369ac8e46a0b3c4ec)
    
    Resolves: rhbz#1477754

--- src/rgw/rgw_rados.h
+++ src/rgw/rgw_rados.h
@@ -1780,6 +1780,7 @@ class RGWRados
   friend class RGWDataSyncProcessorThread;
   friend class RGWStateLog;
   friend class RGWReplicaLogger;
+  friend class RGWCompleteMultipart;
 
   /** Open the pool used as root for this gateway */
   int open_root_pool_ctx();
